fa4fca05f3500946c113e2777f19a3497ca10d04
Akira K <kira@kirameister.net>
2023-08-24 01:23:30 +0200

    normalize both kanji-occurr list and n-gram list with log10 -- this is not to say log10() is the way to go.. I'm still trying to figure out..

---
+++ scripts/kanji_ngram_normalization.py
@@ -0,0 +1,28 @@
+import json
+import numpy as np
+
+kanji_data_norm = dict()
+ngram_data_norm = dict()
+
+with open('data/wikipedia_joyo_kanji_freq.json') as f:
+    kanji_data = json.load(f)
+
+# {"年": [5744064,"1"]},
+for kanji_d in kanji_data:
+    for kanji, vals in kanji_d.items():
+        kanji_data_norm[kanji] = np.log10(vals[0]) # this really doesn't need to be log10
+
+with open('data/wikipedia_joyo_kanji_freq_normalized.json', 'w') as f:
+    json.dump(kanji_data_norm, f, indent=4, ensure_ascii=False)
+
+
+with open('data/n_gram_to_weight.json') as f:
+    ngram_data = json.load(f)
+
+# [["日本",594779],
+# ["現在",246779],
+for ngram in ngram_data:
+   ngram_data_norm[ngram[0]] = np.log10(ngram[1])
+
+with open('data/n_gram_to_weight_normalized.json', 'w') as f:
+    json.dump(ngram_data_norm, f, indent=4, ensure_ascii=False)
